f52bb7a8695bc7e5eb24527b7be46cf8f7172e78
Daniel Pereira <daniel.si@outlook.com.br>
2023-03-29 21:25:48 -0300

    Criando eurecaserver

---
+++ Eurecaserver/src/main/java/oi/github/daniel/eurecaserver/EurecaserverApplication.java
@@ -0,0 +1,13 @@
+package oi.github.daniel.eurecaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurecaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurecaserverApplication.class, args);
+	}
+
+}
---
+++ Eurecaserver/src/test/java/oi/github/daniel/eurecaserver/EurecaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package oi.github.daniel.eurecaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurecaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
